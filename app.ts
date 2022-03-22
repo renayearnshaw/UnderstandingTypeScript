e4d69d99ab5d63e80aab7948c3f67ed5416a883d
Renay Earnshaw <renayearnshaw@gmail.com>
2022-03-22 08:41:40 +0000

    Added comments

--- app.ts
+++ app.ts
@@ -1,7 +1,9 @@
+// types are assigned to the parameters
 function add(n1: number, n2: number) {
     return n1 + n2
 }
 
+// types are inferred from the values used in the assignments
 const number1 = 5
 const number2 = 2.5
 
