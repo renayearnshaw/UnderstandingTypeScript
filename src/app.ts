5421df779b2b09d4aced0678d7a5a8985dc12647
Renay Earnshaw <renayearnshaw@gmail.com>
2022-05-11 08:33:29 +0100

    Classes - the 'this' keyword

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,20 @@
-const person = {
-    name: 'Renay Earnshaw',
-    age: 30
+class Department {
+    name: string
+
+    constructor(name: string) {
+        this.name = name
+    }
+
+    describe() {
+        console.log(`Department is called ${this.name}`)
+    }
 }
 
-console.log(`Hello ${person.name}`)
+const accounting = new Department('accounting')
+
+accounting.describe()
+
+const accountingCopy = { describe: accounting.describe }
+// This will print 'Department is called undefined' because the calling object -
+// accountingCopy - doesn't have a name property
+accountingCopy.describe()
