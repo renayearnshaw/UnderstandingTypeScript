e3404d0ecc830481a3fa1dc8e9d4f79d9712e360
Renay Earnshaw <renayearnshaw@gmail.com>
2022-05-18 08:13:05 +0100

    Marking parameters as readonly

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,37 @@
 class Department {
+    private employees: string[] = []
 
     // Use constructor shorthand to create and assign class properties
     // from constructor params by adding access modifiers.
-    // These are called 'Parameter Properties'
-    constructor(private name: string) {
+    // These are called 'Parameter Properties'.
+    // We also mark the id as a readonly property - it cannot be 
+    // changed after initialisation
+    constructor(private readonly id: string, private name: string) {
     }
 
     // 'this' should always refer to an instance of type Department
     describe(this: Department) {
-        console.log(`Department is called ${this.name}`)
+        console.log(`Department (${this.id}): ${this.name}`)
+    }
+
+    addEmployee(employee: string) {
+        // This will error: Cannot assign to 'id' because it is a read-only property
+        // this.id = 'a33'
+        this.employees.push(employee);
+    }
+
+    printEmployeeInformation() {
+        console.log(this.employees.length);
+        console.log(this.employees);
     }
 }
 
-const accounting = new Department('accounting')
+const accounting = new Department('a54', 'accounting')
+
+accounting.addEmployee('Max');
+accounting.addEmployee('Manu');
 
 accounting.describe()
+accounting.printEmployeeInformation();
 
 const accountingCopy = { name: 'New Department',  describe: accounting.describe }
-// We get a compile error now because the name property is private
-// accountingCopy.describe()
