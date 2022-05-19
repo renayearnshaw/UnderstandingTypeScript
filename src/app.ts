16ff24a25c755358ab1412e954f009e8da15ec9f
Renay Earnshaw <renayearnshaw@gmail.com>
2022-05-19 08:29:59 +0100

    Amended to follow course code exactly

--- src/app.ts
+++ src/app.ts
@@ -1,56 +1,72 @@
 class Department {
     protected employees: string[] = []
 
-    // Use constructor shorthand to create and assign class properties
-    // from constructor params by adding access modifiers.
-    // These are called 'Parameter Properties'.
-    // We also mark the id as a readonly property - it cannot be 
-    // changed after initialisation
     constructor(private readonly id: string, private name: string) {
     }
 
-    // 'this' should always refer to an instance of type Department
     describe(this: Department) {
         console.log(`Department (${this.id}): ${this.name}`)
     }
 
     addEmployee(employee: string) {
-        // This will error: Cannot assign to 'id' because it is a read-only property
-        // this.id = 'a33'
-        this.employees.push(employee);
+        this.employees.push(employee)
     }
 
     printEmployeeInformation() {
-        console.log(this.employees.length);
-        console.log(this.employees);
+        console.log(this.employees.length)
+        console.log(this.employees)
     }
 }
 
 class ITDepartment extends Department {
     admins: string[]
     constructor(id: string, admins: string[]) {
-        super(id, 'IT')
-        // Any assignments to this must be done after the super has been called.
-        // We could use a parameter property here and it would achieve the same thing.
-        this.admins = admins
-    }
-
-    addEmployee(employee: string) {
-        if (employee === 'Renay') {
-            // Renay isn't suitable - she's too old :)
-            return
-        }
-        // employees must be protected, not private, for this to be allowed
-        this.employees.push(employee);
+      super(id, 'IT')
+      this.admins = admins
     }
 }
+  
+class AccountingDepartment extends Department {
 
-const it = new ITDepartment('a54', ['Max'])
+    constructor(id: string, private reports: string[]) {
+      super(id, 'Accounting')
+    }
+  
+    addEmployee(name: string) {
+      if (name === 'Max') {
+        // Max is not suitable for the job
+        return
+      }
+      this.employees.push(name)
+    }
+  
+    addReport(text: string) {
+      this.reports.push(text)
+    }
+  
+    printReports() {
+      console.log(this.reports)
+    }
+}
+  
+const it = new ITDepartment('d1', ['Max'])
 
-it.addEmployee('Renay');
-it.addEmployee('Manu');
+it.addEmployee('Max')
+it.addEmployee('Manu')
+
+// it.employees[2] = 'Anna'
 
 it.describe()
-it.printEmployeeInformation();
+it.printEmployeeInformation()
 
 console.log(it)
+
+const accounting = new AccountingDepartment('d2', [])
+
+accounting.addReport('Something went wrong...')
+
+accounting.addEmployee('Max')
+accounting.addEmployee('Manu')
+
+accounting.printReports()
+accounting.printEmployeeInformation()
