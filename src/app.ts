f0149ba40fe2beb19d09f39f96f899cf0daa703e
Renay Earnshaw <renayearnshaw@gmail.com>
2022-07-19 11:52:36 +0100

    A first (class) decorator

--- src/app.ts
+++ src/app.ts
@@ -1,47 +1,17 @@
-// Create a function type using an interface.
-// Interfaces are used to define the structure of an object,
-// but functions ARE objects, and can thus also be defined with an interface
-// type AddFn =  (a: number, b: number) => number
-interface AddFn {
-    // Creates an ANONYMOUSE function definiton.
-    // Same as the greet method definiton below, but without a method name
-    (a: number, b: number): number
+function Logger(constructor: Function) {
+  console.log("Logging...");
+  console.log(constructor)
 }
 
-let addFn: AddFn
+@Logger
+class Person {
+  name = "Max";
 
-addFn = (n1: number, n2: number) => {
-    return n1 + n2
+  constructor() {
+    console.log("Creating person object...");
+  }
 }
 
-interface Named {
-    // all properties are optional
-    readonly name?: string
-    outputName?: string
-}
+const person = new Person();
 
-interface Greetable extends Named {
-    greet(phrase: string): void
-}
-
-class Person implements Greetable {
-    // name is optional - it might not be set in the constructor
-    readonly name?: string
-
-    constructor(name: string = 'default_name', private age: number) {
-        this.name = name
-    }
-
-    greet(phrase: string) {
-        console.log(`${phrase} ${this.name} and I'm ${this.age}`)
-    }
-}
-
-let user1: Greetable
-
-user1 = new Person(undefined, 21)
-// won't compile
-// user1.name = 'Phil'
-
-user1.greet("Hello, I'm")
-console.log(user1)
+console.log(person);
