390320ba540c65bf3a96576518e292f99b23d903
Renay Earnshaw <renayearnshaw@gmail.com>
2022-07-19 15:54:36 +0100

    A decorator that inserts a static HTML tag

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,16 @@
 // A factory that returns a decorator function
-function Logger(logString: string) {
-  return function (constructor: Function) {
-    console.log(logString);
-    console.log(constructor);
+function WithTemplate(template: string, hookId: string) {
+  return function (_: Function) {
+    const hookElement = document.getElementById(hookId);
+    if (hookElement) {
+      hookElement.innerHTML = template;
+    }
   };
 }
 
 // Executing the function returns a decorator
 // that uses the string passed in
-@Logger('LOGGING PERSON CLASS')
+@WithTemplate('<h1>My Person object</h1>', 'app')
 class Person {
   name = 'Max';
 
