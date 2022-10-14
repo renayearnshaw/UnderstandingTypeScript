40042caa0bdf97cbcb16dd18ad6c1d18c0f7ead9
Renay Earnshaw <renayearnshaw@gmail.com>
2022-10-14 13:55:17 +0100

    Add course object without validation

--- src/app.ts
+++ src/app.ts
@@ -75,3 +75,26 @@ class Product {
     return this._price * (1 + tax);
   }
 }
+
+class Course {
+  title: string;
+  price: number;
+
+  constructor(title: string, price: number) {
+    this.title = title;
+    this.price = price;
+  }
+}
+
+const courseForm = document.querySelector('form')!;
+courseForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const titleEl = document.getElementById('title') as HTMLInputElement;
+  const priceEl = document.getElementById('price') as HTMLInputElement;
+
+  const title = titleEl.value;
+  const price = +priceEl.value;
+
+  const createdCourse = new Course(title, price);
+  console.log(createdCourse);
+});
